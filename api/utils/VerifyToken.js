af94ddae662a23e67c226068868b4a60c674cec4
27pavann <stud.2201201098@kpgu.ac.in>
2025-08-19 16:44:16 +0530

    added

--- api/utils/VerifyToken.js
+++ api/utils/VerifyToken.js
@@ -1,16 +1,22 @@
+import jwt from "jsonwebtoken";
+import { errorHandler } from "./error.js";
 
-import jwt from 'jsonwebtoken';
-import { errorHandler } from './error.js';
+export const VerifyToken = (req, res, next) => {
+  // accept cookie or Authorization: Bearer <token>
+  const token =
+    req.cookies?.access_token ||
+    (req.headers.authorization &&
+      req.headers.authorization.startsWith("Bearer ") &&
+      req.headers.authorization.split(" ")[1]);
 
-export const VerifyToken = (req, res , next) => {
-  const token = req.cookies.access_token;
+  if (!token) return next(errorHandler(401, "Unauthorized"));
 
-  if (!token) return next(errorHandler(401, 'Unauthorized'));
-
-  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
-    if (err) return next(errorHandler(403, 'Forbidden'));
-
-    req.user = user;
-    next();
-  });
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded;
+    return next();
+  } catch (err) {
+    console.error("JWT verify error:", err);
+    return next(errorHandler(403, "Forbidden"));
+  }
 };
