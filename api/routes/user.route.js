f8475f40f2ffa2cd3817d62a237afd41ce3abdca
27pavann <stud.2201201098@kpgu.ac.in>
2025-11-08 00:55:34 +0530

    added

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -3,9 +3,9 @@ import {
   test,
   uploadAvatar,
   updateUser,
+  deleteUser,
 } from "../controllers/user.controller.js"; // Importing the test controller
 import { VerifyToken } from "../utils/VerifyToken.js";
-import { deleteUser } from "../controllers/auth.controller.js";
 const router = express.Router();
 
 router.get("/test", test);
