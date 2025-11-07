daf1d15b36c2b9b20f1369b526b5d44f305655cc
27pavann <stud.2201201098@kpgu.ac.in>
2025-11-07 23:14:16 +0530

    added

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,15 +1,11 @@
 import express from "express";
-import { test } from "../controllers/user.controller.js"; // Importing the test controller
-import {
-  updateUser,
-  uploadProfileImage,
-  deleteUser,
-} from "../controllers/auth.controller.js";
+import { test, uploadAvatar } from "../controllers/user.controller.js"; // Importing the test controller
+import { updateUser, deleteUser } from "../controllers/auth.controller.js";
 import { VerifyToken } from "../utils/VerifyToken.js";
 const router = express.Router();
 
 router.get("/test", test);
-router.post("/upload/:id", VerifyToken, uploadProfileImage);
+router.post("/upload/:id", VerifyToken, uploadAvatar);
 router.post("/update/:id", VerifyToken, updateUser);
 router.delete("/delete/:id", VerifyToken, deleteUser);
 
